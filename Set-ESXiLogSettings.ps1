729f5fd09d8c450198f9e934d1b3f6fb3fb41539
hsosa1966 <47432735+hsosa1966@users.noreply.github.com>
2019-08-21 22:40:35 -0400

    Add files via upload

---
+++ Set-ESXiLogSettings.ps1
@@ -0,0 +1,7 @@
+﻿$SettingName = "Config.HostAgent.log.level","Vpx.Vpxa.config.log.level","Vpx.Vpxa.config.log.memoryLevel"
+
+Get-VMHost | Where-Object {$_.ConnectionState -eq "Connected"} | Get-AdvancedSetting -Name $SettingName |Sort Entity | Select Entity,Name,Value | Format-Table -AutoSize
+
+Get-VMHost | Where-Object {$_.ConnectionState -eq "Connected"} | Get-AdvancedSetting -Name $SettingName | Set-AdvancedSetting -Confirm:$false -Value info
+
+Get-VMHost | Where-Object {$_.ConnectionState -eq "Connected"} | Get-AdvancedSetting -Name $SettingName |Sort Entity | Select Entity,Name,Value | Format-Table -AutoSize
